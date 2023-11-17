1bed3779fe9e4dfa54f24a65f9551b1cc65a7e56
SirHektor <heitorcosta.aredes@Gmail.com>
2023-11-17 10:28:41 -0300

    mudanças e configurações no código do butão adição de uma nova imagem de fundo

--- main.js
+++ main.js
@@ -1,28 +1,33 @@
 document.addEventListener('DOMContentLoaded', function () {
-    // Data do Natal
     const christmasDate = new Date('December 25, 2023 00:00:00').getTime();
 
-    // Atualiza o contador a cada segundo
     const countdown = setInterval(function () {
         const now = new Date().getTime();
         const distance = christmasDate - now;
 
-        // Calcula dias, horas, minutos e segundos restantes
         const days = Math.floor(distance / (1000 * 60 * 60 * 24));
         const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
         const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
         const seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
-        // Atualiza o HTML com os valores
         document.getElementById('days').innerHTML = days + 'd';
         document.getElementById('hours').innerHTML = hours + 'h';
         document.getElementById('minutes').innerHTML = minutes + 'm';
         document.getElementById('seconds').innerHTML = seconds + 's';
 
-        // Verifica se a data do Natal foi atingida
         if (distance < 0) {
             clearInterval(countdown);
-            document.getElementById('countdown').innerHTML = 'Merry Christmas!';
+            document.getElementById('countdown').innerHTML = 'Feliz Natal!';
         }
     }, 1000);
+
+    const button = document.querySelector('.countdown-button');
+    const countdownContainer = document.querySelector('#countdown');
+
+    button.addEventListener('click', function () {
+        // Oculta o botão
+        button.style.display = 'none';
+        // Mostra o container do temporizador
+        countdownContainer.style.display = 'block';
+    });
 });
